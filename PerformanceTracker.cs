d7710e53b8d6e92750e10c923870c3aa8eee3652
Davydson Santana <davys.fellipe@gmail.com>
2022-07-15 11:16:02 -0300

    feature: add inserction sort algorithm

---
+++ PerformanceTracker.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms {
+    public class PerformanceTracker : IDisposable {
+
+        public readonly string Algorithm;
+        public readonly string Detail;
+        private int GCGen0Before;
+        private int GCGen0After;
+        private long MemoryBefore;
+        private long MemoryAfter;
+        private Stopwatch SW;
+
+        public long ElapsedMilliseconds { get { return SW.ElapsedMilliseconds; } }
+        public long MemoryConsumption { get { return MemoryAfter - MemoryBefore; } }
+        public long GCGen0Counter { get { return GCGen0After - GCGen0Before; } }
+
+
+        internal PerformanceTracker(string algorithm, string detail) {
+            Log("Algorithm", $"{algorithm} {detail}");
+            Algorithm = algorithm;
+            Detail = detail;
+            GCGen0Before = GC.CollectionCount(0);
+            MemoryBefore = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024;
+            SW = Stopwatch.StartNew();
+        }
+
+        public static PerformanceTracker StartTrack(string algorithm, string detail) {            
+            var performanceTracker = new PerformanceTracker(algorithm, detail);            
+            return performanceTracker;
+        }
+
+        public void Stop() {
+            SW.Stop();
+            MemoryAfter = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024;
+            GCGen0After = GC.CollectionCount(0);
+            Log("Time", $"{ElapsedMilliseconds} ms");
+            Log("#Gen0", $"{GCGen0Counter}");
+            Log("#Mem", $"{MemoryConsumption}");
+            Console.WriteLine("");
+        }
+        
+        private void Log(string key, string value) {
+            Console.WriteLine($"{string.Format("{0, 15}", key)} .: {value}");
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Sort/InserctionSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sort {
+    /// <summary>
+    /// An efficient algorithm for sorting a small number of elements
+    /// </summary>
+    public class InserctionSort {
+
+        /// <summary>
+        /// Sort values
+        /// </summary>
+        /// <param name="array">Values to be sorted</param>
+        /// <param name="n">Array count</param>
+        /// <returns></returns>
+        public static int[] Sort(int[] A, int n) {
+
+            // Performance measure
+            /*
+            Console.WriteLine($"Algorithm .: InserctionSort with {n} keys");
+            var garbageBefore = GC.CollectionCount(0);
+            var memoryBefore = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024;
+            var sw = Stopwatch.StartNew();
+            */
+
+            var PT = PerformanceTracker.StartTrack("InserctionSort", $"with {n} keys");
+
+            // Algorithm
+            for (int i = 1; i < n; i++) {
+                var key = A[i];
+
+                // Insert A[i] into de sorted subarray A[1:i-1]
+                var j = i - 1;
+                while(j>=0 && A[j] > key) {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                    A[j + 1] = key;
+                }
+            }
+
+            // Performance measure
+            PT.Stop();
+
+            /*
+            sw.Stop();
+            var memoryAfter = Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024;
+            
+            Console.WriteLine($"Time      .: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine($"#Gen0     .: {GC.CollectionCount(0) - garbageBefore}");
+            Console.WriteLine($"#Mem      .: {memoryAfter - memoryBefore}");
+            Console.WriteLine("");
+            */
+            return A;
+        }
+
+        public static void TestSort() {
+            for (int i = 1; i <= 6; i++) {
+                var arrSize = Convert.ToInt32(Math.Pow(10, i));
+                var arr = new int[arrSize];
+                for (int k = 0; k < arrSize; k++) {
+                    arr[arrSize - 1 - k] = k + 1;
+                }
+                var sortedArr = Sort(arr, arrSize);
+            }
+        }
+
+    }
+}
+
